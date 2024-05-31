eb1e31c71f696911895d608dfd7d0be27e8bb28d
khushi303 <maankhushi303@gmail.com>
2024-05-31 13:58:12 +0530

    adding dashboard

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import logo from "../assets/images/png/logo.png";
-import { Link, NavLink } from "react-router-dom";
+import { Link, NavLink, useLocation } from "react-router-dom";
 import { sideTabs } from "./common/Helper";
 import { MessageIcon, Plus } from "./common/Icons";
 
 const SideBar = () => {
+  let location = useLocation();
   return (
     <div>
       {" "}
       <div
-        className={` z-50 content lg:w-[253px] sm:w-[180px] w-[60px] transition-all duration-300 ease-linear h-screen overflow-y-scroll bg-white`}
+        className={` z-50 content lg:w-[253px]  transition-all duration-300 ease-linear h-screen overflow-y-scroll bg-white`}
       >
         <NavLink to={"/"} className="py-6 pl-6 pr-[29px]">
           <img
@@ -26,12 +27,10 @@ const SideBar = () => {
               <NavLink
                 key={index}
                 to={value.link}
-                className={({ isActive }) =>
-                  `${
-                    isActive &&
-                    "bg-opacity-100 after:top-0 after:!h-full !text-Christine bg-[#FEF8F3] after:duration-300 duration-300 "
-                  } bg-opacity-0 relative after:right-0 after:rounded-md flex gap-2 py-3 pl-8 transition-all ease-linear duration-300 after:absolute group hover:text-Christine after:h-0 after:duration-300 after:w-1 after:bg-Christine bg-white text-sm text-black font-medium items-center hover:bg-[#FEF8F3]`
-                }
+                className={`${
+                  location.pathname === value.link &&
+                  "bg-opacity-100 after:top-0 after:!h-full !text-Christine bg-[#FEF8F3] after:duration-300 duration-300 "
+                } bg-opacity-0 relative after:right-0 after:rounded-md flex gap-2 py-3 pl-8 transition-all ease-linear duration-300 after:absolute group hover:text-Christine after:h-0 after:duration-300 after:w-1 after:bg-Christine bg-white text-sm text-black font-medium items-center hover:bg-[#FEF8F3]`}
               >
                 {value.icon}
                 {value.title}
--- src/pages/Angebote.jsx
+++ src/pages/Angebote.jsx
@@ -3,16 +3,16 @@ import DashboardHeader from "../components/DashboardHeader";
 import SideBar from "../components/SideBar";
 import DashboardContent from "../components/DashboardContent";
 
-const Angebote = () => {
+const Buchungen = () => {
   return (
     <>
       <div className="max-w-[1920px] mx-auto flex min-h-screen overflow-hidden">
         <SideBar />
-        <div className="min-h-screen relative lg:w-[calc(100%-256px)] sm:w-[calc(100%-180px)] w-[calc(100%-60px)] ml-auto bg-white">
-          <div className="lg:h-[85px] sm:h-[60px] h-[40px] bg-white">
+        <div className="min-h-screen relative lg:w-[calc(100%-256px)] w-full ml-auto bg-white">
+          <div className="h-[104px] bg-white">
             <DashboardHeader />
           </div>
-          <div className="lg:min-h-[calc(100vh-104px)] content sm:max-h-[calc(100vh-60px)] max-h-[calc(100vh-40px)] w-full overflow-auto">
+          <div className="min-h-[calc(100vh-104px)] content w-full overflow-auto">
             <DashboardContent />
           </div>
         </div>
@@ -21,4 +21,4 @@ const Angebote = () => {
   );
 };
 
-export default Angebote;
+export default Buchungen;
